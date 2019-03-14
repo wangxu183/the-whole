2bc9a4355eeac3378d5116043b5fd459e6b13b4b
student <student@hubu.edu.cn>
2019-03-14 16:43:59 +0800

    awet

---
+++ goodapp-init/src/components/Login.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="container" >
+    <x-header :left-options="{showBack: true}" :right-options="{showMore: false}">登录</x-header>
+    <group title="用户登录">
+     <x-input title="手机号码" mask="999 9999 9999" :max="13" is-type="china-mobile"></x-input>
+     <x-input title="密码"  :max="13" type="password"></x-input>
+    </group>
+    <div class="btn">
+     <x-button @click.native="Login">登录 </x-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import {XInput, XButton, XHeader, Group, Cell, Alert, AlertModule} from 'vux'
+export default {
+  components: {
+    XInput,
+    XButton,
+    XHeader,
+    Group,
+    Cell,
+    Alert
+  },
+  methods: {
+    Login () {
+      if (this.password === '123456') {
+        this.$router.push({path: '/'})
+      } else {
+        AlertModule.show({
+          title: '登录失败',
+          content: '密码错误'
+        })
+      }
+    }
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container {
+    padding: 20pt;
+}
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ goodapp-init/src/components/home.vue
@@ -0,0 +1,105 @@
+<template>
+  <div>
+ <x-header :left-options="{showBack: false}"  @on-click-more="Login" :right-options="{showMore: true}">Eating</x-header>
+  <swiper loop auto :list="demo06_list" :index="demo06_index" @on-index-change="demo06_onIndexChange"></swiper>
+      <grid :cols="4" :show-lr-borders="false">
+      <grid-item :label="('苹果')">
+        <img slot="icon" src="../assets/app.png">
+      </grid-item>
+      <grid-item :label="('橙子')">
+        <img slot="icon" src="../assets/app.png">
+      </grid-item>
+      <grid-item :label="('葡萄')">
+        <img slot="icon" src="../assets/app.png">
+      </grid-item>
+      <grid-item :label="('榴莲')">
+        <img slot="icon" src="../assets/app.png">
+      </grid-item>
+      <grid-item :label="('草莓')">
+        <img slot="icon" src="../assets/app.png">
+      </grid-item>
+      <grid-item :label="('芒果')">
+        <img slot="icon" src="../assets/app.png">
+      </grid-item>
+      <grid-item :label="('布丁')">
+        <img slot="icon" src="../assets/app.png">
+      </grid-item>
+      <grid-item :label="('奶茶')">
+        <img slot="icon" src="../assets/app.png">
+      </grid-item>
+    </grid>
+    <group :title="('Switch the type')">
+      <radio title="type" v-model="type" :options="['1', '2', '3', '4', '5']"></radio>
+    </group>
+    <panel :header="('List of content with image')" :footer="footer" :list="list" :type="type" @on-img-error="onImgError"></panel>
+    <tabbar>
+      <tabbar-item>
+        <img slot="icon" src="../assets/home.png">
+        <span slot="label">all</span>
+      </tabbar-item>
+      <tabbar-item show-dot>
+        <img slot="icon" src="../assets/book.png">
+        <span slot="label">fruit</span>
+      </tabbar-item>
+      <tabbar-item  >
+        <img slot="icon" src="../assets/map.png">
+        <span slot="label">around</span>
+      </tabbar-item>
+      <tabbar-item badge="2">
+        <img slot="icon" src="../assets/mine.png">
+        <span slot="label">mine</span>
+      </tabbar-item>
+    </tabbar>
+  </div>
+</template>
+
+<script>
+import {Tabbar, TabbarItem, Group, Cell, XHeader, Swiper, Grid, GridItem, Panel, Radio} from 'vux'
+const baseList = [{
+  url: 'javascript:',
+  img: 'https://ww1.sinaimg.cn/large/663d3650gy1fq66vvsr72j20p00gogo2.jpg',
+  title: '送你一朵fua'
+}, {
+  url: 'javascript:',
+  img: 'https://ww1.sinaimg.cn/large/663d3650gy1fq66vw1k2wj20p00goq7n.jpg',
+  title: '送你一辆车'
+}, {
+  url: 'javascript:',
+  img: 'https://static.vux.li/demo/5.jpg', // 404
+  title: '送你一次旅行',
+  fallbackImg: 'https://ww1.sinaimg.cn/large/663d3650gy1fq66vw50iwj20ff0aaaci.jpg'
+}]
+
+const urlList = baseList.map((item, index) => ({
+  url: 'http://m.baidu.com',
+  img: item.img,
+  fallbackImg: item.fallbackImg,
+  title: `(可点击)${item.title}`
+}))
+
+export default {
+  components: {
+    Radio,
+    Panel,
+    Grid,
+    GridItem,
+    Swiper,
+    XHeader,
+    Tabbar,
+    TabbarItem,
+    Group,
+    Cell
+  },
+  methods: {
+    Login () {
+      this.$router.push('/Login')
+    }
+  },
+  data () {
+    return {
+      demo06_list: urlList,
+      demo06_index: 0
+    }
+  }
+}
+</script>
